756d97d75610827d419c576f202f4dd68f0bdef6
Oigbochie Blessed <eprince1800@gmail.com>
2024-03-07 22:10:36 +0100

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const Navbar = async (props: Props) => {
+    return(
+        <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-s-zinc-300 py-2">
+            <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl"
+            >
+                <Link href={"./"} className="flex items-center gap-2">
+                    <p className="rounded-lg border-2 border-b-4 border-r-4 border-black text-xl font-bold text-s-zinc-900 dark:text-s-zinc-100 transition-all hover:-translate-y-[2px] md:block dark:border-white px-3">Git-stat</p>
+                </Link>
+            </div>
+        </div>
+    )
+};
+
+export default Navbar;
