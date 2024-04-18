a3524d32b139c25713cb068a955419118485647d
Oigbochie Blessed <eprince1800@gmail.com>
2024-04-18 18:55:05 +0100

    fixed up the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,27 +13,25 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="flex justify-between align-middle">
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
       <Card className="max-w-3/4 md:max-w-[500px]">
         <CardHeader>
           <CardTitle>Welcome to Git stat</CardTitle>
           <CardDescription>Get git info</CardDescription>
         </CardHeader>
         <CardContent className="grid md:flex justify-center align-middle gap-2">
-          <CardDescription>
-            This website displays a card of github user account information
-            using the username
-          </CardDescription>
-          we utilise the github api using Octokit to fetch the userdata
-          <Button
-            className="w-4 h-4"
-            onClick={() => {
-              router.push("/dashboard");
-            }}
-          >
-            Get started
-          </Button>
+          This website displays a card of github user account information using
+          the username we utilise the github api using Octokit to fetch the
+          userdata
         </CardContent>
+        <Button
+          className="max-w-full self-center mb-2 mx-4"
+          onClick={() => {
+            router.push("/dashboard");
+          }}
+        >
+          Get started
+        </Button>
       </Card>
     </div>
   );
